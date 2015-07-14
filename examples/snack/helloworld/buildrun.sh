6cfa9f86314593dded6ec69b3aacb383d19ff4c1
Greg Rodgers <grodgers@purdue.edu>
2015-07-14 08:52:58 -0500

    cleanup example buildrun.sh don't need env vars, don't need -lelf

--- examples/snack/helloworld/buildrun.sh
+++ examples/snack/helloworld/buildrun.sh
@@ -6,36 +6,61 @@
 #  ./buildrun cpp
 #  ./buildrun f
 #
+function getdname(){
+   local __DIRN=`dirname "$1"`
+   if [ "$__DIRN" = "." ] ; then 
+      __DIRN=$PWD; 
+   else
+      if [ ${__DIRN:0:1} != "/" ] ; then 
+         if [ ${__DIRN:0:2} == ".." ] ; then 
+               __DIRN=`dirname $PWD`/${__DIRN:3}
+         else
+            if [ ${__DIRN:0:1} = "." ] ; then 
+               __DIRN=$PWD/${__DIRN:2}
+            else
+               __DIRN=$PWD/$__DIRN
+            fi
+         fi
+      fi
+   fi
+   echo $__DIRN
+}
 
-#  Set HSA Environment variables
-[ -z $HSA_RUNTIME_PATH ] && HSA_RUNTIME_PATH=/opt/hsa
-[ -z $HSA_LLVM_PATH ] && HSA_LLVM_PATH=/opt/amd/cloc/bin
-export LD_LIBRARY_PATH=$HSA_RUNTIME_PATH/lib
+example_dir=$(getdname $0)
 
-#  First compile the acclerated functions to create hw.o
+#  Compile the acclerated functions with snack.sh to create hw.o
+echo 
+echo "cd $example_dir"
+cd $example_dir
+
+export LD_LIBRARY_PATH=/opt/hsa/lib
+
+#  Compile the acclerated functions with snack.sh to create hw.o
 echo 
 if [ "$1" == "f" ] ; then 
-   echo $HSA_LLVM_PATH/snack.sh -q -fort -c hw.cl 
-   $HSA_LLVM_PATH/snack.sh -q -fort -c hw.cl 
+   echo "snack.sh -fort -c hw.cl"
+   snack.sh -fort -c hw.cl 
 else
-   echo $HSA_LLVM_PATH/snack.sh -q -c hw.cl 
-   $HSA_LLVM_PATH/snack.sh -q -c hw.cl 
+   echo "snack.sh -c hw.cl "
+   snack.sh -c hw.cl 
 fi
 
 #  Compile the main program and link to hw.o
 #  Main program can be c, cpp, or fotran
 echo 
 if [ "$1" == "cpp" ] ; then 
-   echo "g++ -o HelloWorld hw.o HelloWorld.cpp -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64  "
-   g++ -o HelloWorld hw.o HelloWorld.cpp -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64  
+   echo "g++ -o HelloWorld hw.o HelloWorld.cpp -L/opt/hsa/lib -lhsa-runtime64"
+   g++ -o HelloWorld hw.o HelloWorld.cpp -L/opt/hsa/lib -lhsa-runtime64  
 elif [ "$1" == "f" ] ; then 
-   echo "f95 -o HelloWorld hw.o HelloWorld.f -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64  "
-   f95 -o HelloWorld hw.o HelloWorld.f -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 
+   echo "f95 -o HelloWorld hw.o HelloWorld.f -L/opt/hsa/lib -lhsa-runtime64"
+   f95 -o HelloWorld hw.o HelloWorld.f -L/opt/hsa/lib -lhsa-runtime64  
 else
-   echo "gcc -o HelloWorld hw.o HelloWorld.c -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 "
-   gcc -o HelloWorld hw.o HelloWorld.c -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 
+   echo "gcc -o HelloWorld hw.o HelloWorld.c -L/opt/hsa/lib -lhsa-runtime64"
+   gcc -o HelloWorld hw.o HelloWorld.c -L/opt/hsa/lib -lhsa-runtime64 
 fi
 
+# Run the program
 echo 
 echo ./HelloWorld
 ./HelloWorld
+
