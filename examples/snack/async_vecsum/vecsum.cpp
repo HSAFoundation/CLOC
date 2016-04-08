cb674a186db936d85ea896bcc184fcf69ccd3764
Greg Rodgers <grodgers@purdue.edu>
2016-04-07 23:46:32 -0500

    use hsa_memory_allocate for async_vesum example

--- examples/snack/async_vecsum/vecsum.cpp
+++ examples/snack/async_vecsum/vecsum.cpp
@@ -8,6 +8,7 @@
 #include "sumKernel.h"
 #define NSECPERSEC 1000000000L
 #define VECLEN 102500000
+//#define VECLEN 536870912
 #define SUMS_PER_WKG 8192
 #define WKG_SIZE 512
 #define INT_TYPE int 
@@ -23,6 +24,12 @@ long int get_nanosecs( struct timespec start_time, struct timespec end_time) {
    return nanosecs;
 }
 
+void sumrem(int N, INT_TYPE*invec, INT_TYPE * result){
+   INT_TYPE sum=0;
+   for(int i=0; i<N; i++) sum +=invec[i];
+   *result=sum;
+}
+
 int main(int argc, char **argv) {
 
    struct timespec start_time[6],end_time[6];
@@ -32,12 +39,13 @@ int main(int argc, char **argv) {
    int vec2_len = ((vec1_len-1)/SUMS_PER_WKG) + 1; 
    INT_TYPE result;
    INT_TYPE *invec,*outvec1, *outvec2;
-   int rc=posix_memalign((void**)&invec,8192,VECLEN*sizeof(INT_TYPE));
-   rc = posix_memalign((void**)&outvec1,8192,vec1_len*sizeof(INT_TYPE));
-   rc = posix_memalign((void**)&outvec2,8192,vec2_len*sizeof(INT_TYPE));
+   int vecrem;
+   invec = (INT_TYPE*) malloc_global(VECLEN*sizeof(INT_TYPE));
+   outvec1 = (INT_TYPE*) malloc_global(vec1_len*sizeof(INT_TYPE));
+   outvec2 = (INT_TYPE*) malloc_global(vec2_len*sizeof(INT_TYPE));
    
    /* Initialize */
-   for(int i=0; i<VECLEN; i++)  invec[i]=2; 
+   for(int i=0; i<VECLEN; i++)  invec[i]= (INT_TYPE) 2; 
 
 
    int kcalls = vec1_len + vec2_len;
@@ -61,13 +69,19 @@ int main(int argc, char **argv) {
    SNK_INIT_LPARM(lparm1,WKG_SIZE);
    lparm1->ldims[0] = WKG_SIZE;
    clock_gettime(CLOCK_MONOTONIC_RAW,&start_time[1]);
-   for(int i=0; i<vec1_len-1; i++) sum8192Kernel(&invec[i*SUMS_PER_WKG],&outvec1[i],lparm1); 
-   sum8192KernelN( ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &invec[(vec1_len-1)*SUMS_PER_WKG],&outvec1[vec1_len-1],lparm1); 
-
-   for(int i=0; i<vec2_len-1; i++)  sum8192Kernel(&outvec1[i*SUMS_PER_WKG],&outvec2[i],lparm1); 
-   sum8192KernelN( ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &outvec1[(vec2_len-1)*SUMS_PER_WKG],&outvec2[vec2_len-1],lparm1); 
+   for(int i=0; i<vec1_len-1; i++) 
+      sum8192Kernel(&invec[i*SUMS_PER_WKG], &outvec1[i], lparm1); 
+   vecrem = ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1;
+   int base=(vec1_len-1)*SUMS_PER_WKG;
+   sumrem(vecrem, &invec[base], &outvec1[vec1_len-1]);
+   for(int i=0; i<vec2_len-1; i++)  
+      sum8192Kernel(&outvec1[i*SUMS_PER_WKG],&outvec2[i],lparm1); 
+   vecrem = ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1;
+   outvec2[vec2_len-1]=0;
+   base=(vec2_len-1)*SUMS_PER_WKG;
+   sumrem(vecrem, &outvec1[base], &outvec2[vec2_len-1]);
    result = 0;
-   for(int i=0; i<vec2_len; i++)  result += outvec2[i]; 
+   for(int i=0; i<vec2_len; i++) result += outvec2[i]; 
    clock_gettime(CLOCK_MONOTONIC_RAW,&end_time[1]);
    nanosecs[1] = get_nanosecs(start_time[1],end_time[1]);
    kps[1] = ((float) kcalls * (float) NSECPERSEC) / (float) nanosecs[1] ;
@@ -81,11 +95,11 @@ int main(int argc, char **argv) {
    clock_gettime(CLOCK_MONOTONIC_RAW,&start_time[2]);
    clock_gettime(CLOCK_MONOTONIC_RAW,&start_time[3]);
    for(int i=0; i<vec1_len-1; i++) sum8192Kernel(&invec[i*SUMS_PER_WKG],&outvec1[i],lparm1); 
-   sum8192KernelN( ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &invec[(vec1_len-1)*SUMS_PER_WKG],&outvec1[vec1_len-1],lparm1); 
+   sumrem( ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &invec[(vec1_len-1)*SUMS_PER_WKG],&outvec1[vec1_len-1]);
    clock_gettime(CLOCK_MONOTONIC_RAW,&end_time[2]);
    stream_sync(0);  /* Wait for all the kernels to complete before next pass */
    for(int i=0; i<vec2_len-1; i++)  sum8192Kernel(&outvec1[i*SUMS_PER_WKG],&outvec2[i],lparm1); 
-   sum8192KernelN( ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &outvec1[(vec2_len-1)*SUMS_PER_WKG],&outvec2[vec2_len-1],lparm1); 
+   sumrem( ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &outvec1[(vec2_len-1)*SUMS_PER_WKG],&outvec2[vec2_len-1]); 
    stream_sync(0);  /* Wait for all the kernels to complete before summing remaining terms */
    result = 0;
    for(int i=0; i<vec2_len; i++)  result += outvec2[i]; 
@@ -115,7 +129,7 @@ int main(int argc, char **argv) {
    }
    if (( vec1_len % 2 ) == 0 )  
       sum8192Kernel(&invec[(vec1_len-2)*SUMS_PER_WKG],&outvec1[vec1_len-2],lparm1); 
-   sum8192KernelN( ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &invec[(vec1_len-1)*SUMS_PER_WKG],&outvec1[vec1_len-1],lparm2); 
+   sumrem( ((VECLEN+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &invec[(vec1_len-1)*SUMS_PER_WKG],&outvec1[vec1_len-1]);
    clock_gettime(CLOCK_MONOTONIC_RAW,&end_time[4]);
    stream_sync(1);  /* Wait for all the kernels to complete before next pass */
    stream_sync(2); 
@@ -125,7 +139,7 @@ int main(int argc, char **argv) {
    }
    if (( vec2_len % 2 ) == 0 )  
       sum8192Kernel(&outvec1[(vec2_len-2)*SUMS_PER_WKG],&outvec2[vec2_len-2],lparm1);  
-   sum8192KernelN( ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &outvec1[(vec2_len-1)*SUMS_PER_WKG],&outvec2[vec2_len-1],lparm2); 
+   sumrem( ((vec1_len+SUMS_PER_WKG-1) % SUMS_PER_WKG)+1, &outvec1[(vec2_len-1)*SUMS_PER_WKG],&outvec2[vec2_len-1]);
    stream_sync(1); 
    stream_sync(2); 
    result = 0;
@@ -142,4 +156,3 @@ int main(int argc, char **argv) {
    printf("  Kernels per sec  =  %10.0f  (KPS) \n\n",kps[5]);
 
 }
-
