3b093fe0791b802b97f48de8cf21d43d4a983014
Ashwin Aji <ashwinma@gmail.com>
2016-04-14 18:42:17 -0500

    changed CPP version of helloworld to use malloc_global

--- examples/snack/helloworld/HelloWorld.cpp
+++ examples/snack/helloworld/HelloWorld.cpp
@@ -4,13 +4,16 @@
 using namespace std;
 #include "hw.h"
 int main(int argc, char* argv[]) {
-	const char* input = "Gdkkn\x1FGR@\x1FVnqkc";
-	size_t strlength = strlen(input);
+	const char* input1 = "Gdkkn\x1FGR@\x1FVnqkc";
+	size_t strlength = strlen(input1);
+	char *input = (char*) malloc_global(strlength + 1);
 	char *output = (char*) malloc_global(strlength + 1);
+	memcpy(input, input1, strlength+1);
         SNK_INIT_LPARM(lparm,strlength);
         decode(input,output,lparm);
 	output[strlength] = '\0';
 	cout << output << endl;
+	free_global(input);
 	free_global(output);
 	return 0;
 
